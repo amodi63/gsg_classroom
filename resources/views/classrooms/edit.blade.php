28633ff36d8fa8fdd1b610729a01b399ad5d783a
amodi63 <kotch2001@gmail.com>
2023-07-15 01:53:34 +0300

    fixes

--- resources/views/classrooms/edit.blade.php
+++ resources/views/classrooms/edit.blade.php
@@ -22,13 +22,20 @@
             <label for="room">Room</label>
           </div>
           <div class="form-floating mb-3">
-            <input class="form-control " type="file" name="classroom_image" id="classroom_image">
+            <input class="form-control" type="file" name="classroom_image" id="classroom_image">
             <label for="classroom_image" class="custom-file-label">Class Image</label>
-          </div>
-          <div class="form-floating mb-3">
-            <input class="form-control " type="file" name="cover_image_path" id="cover_image_path">
-            <label for="cover_image_path" class="custom-file-label">CLass Cover Image</label>
-          </div>
+            @if ($classroom->classroom_image)
+                <img src="{{ asset('storage/'.$classroom->classroom_image) }}" class="img-thumbnail mt-2"  width="150" height="150"   alt="Classroom Image">
+            @endif
+        </div>
+        
+        <div class="form-floating mb-3">
+            <input class="form-control" type="file" name="cover_image_path" id="cover_image_path">
+            <label for="cover_image_path" class="custom-file-label">Class Cover Image</label>
+            @if ($classroom->cover_image_path)
+                <img src="{{ asset('storage/'.$classroom->cover_image_path) }}" class="img-thumbnail mt-2" width="300" height="150"  alt="Cover Image">
+            @endif
+        </div>
           <button type="submit" class="btn btn-primary">Update Classroom</button>
     </form>
   @endsection
